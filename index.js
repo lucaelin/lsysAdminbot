3d85c4944cfa83ae44875e3efbe9ab66e2a8a3a5
Luca Haneklau <git@luca.lsys.ac>
2018-09-03 22:22:01 +0200

    added lowdb.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Telegraf = require('telegraf');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 const callbackButton = require('./callbackButton.js');
 const Resolver = require('./Resolver.js');
 const Help = require('./Help.js');
@@ -7,6 +9,8 @@ const plugins = require('./plugins');
 const config = require('./config.json');
 
 const bot = new Telegraf(config.botToken);
+const db = low(new FileSync('db.json'));
+db.defaults({settings: {}}).write();
 global.bot = bot;
 bot.use((new Resolver(config.resolveToken)).middleware);
 callbackButton(bot);
@@ -15,6 +19,6 @@ new Help(bot);
 bot.telegram.getMe().then((botInfo) => {
   bot.options.username = botInfo.username;
 });
-global.plugins = plugins.loadAll(bot);
+global.plugins = plugins.loadAll(bot, db);
 
 bot.startPolling();
